e2e565685383333ec5a36daf7afbb27ee875fe17
Jane <rejaine.araujo@gmail.com>
2024-03-08 17:30:57 -0500

    removing comments from the next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// module.exports = nextConfig;
-
 const withMDX = require("@next/mdx")();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions`` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
-  // fs to work
-  // webpack: (config, { isServer }) => {
-  //   // Fixes npm packages that depend on `fs` module
-  //   if (!isServer) {
-  //     config.node = {
-  //       fs: "empty",
-  //     };
-  //   }
-
-  //   return config;
-  // },
 };
 
 module.exports = withMDX(nextConfig);
