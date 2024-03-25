877adb20b960a8fcf5c22e88d7e2672008f37338
Jane <rejaine.araujo@gmail.com>
2024-03-25 17:49:26 -0400

    chores: cleaning inline style

--- src/components/appbar/actions.jsx
+++ src/components/appbar/actions.jsx
@@ -1,68 +1,36 @@
-"use client";
+// "use client";
 
-import { MyList } from "@/styles/appbar";
-import { ListItem, ListItemButton } from "@mui/material";
-import { Colors } from "../../styles/theme";
-import Link from "next/link";
+// import { MyList } from "@/styles/appbar";
+// import { ListItem, ListItemButton } from "@mui/material";
+// import { Colors } from "../../styles/theme";
+// import Link from "next/link";
 
-export default function Actions() {
-  return (
-    <MyList type="row" sx={{ position: "absolute", right: "0", pt: 1 }}>
-      <Link href="/log-in">
-        <ListItemButton
-          variant="contained"
-          disableTouchRipple
-          sx={{
-            // justifyContent: "center",
-            // "&.MuiListItemButton-root:hover": {
-            //   color: Colors.green,
-            //   background: "transparent",
-            m: "4px",
-            // },
-          }}>
-          <ListItem
-            sx={
-              {
-                // display: "flex",
-                // justifyContent: "center",
-                // fontWeight: "bold",
-                // color: Colors.white_off, // check the color
-                // background: Colors.green,
-                // borderRadius: "4px",
-                // letterSpacing: "0.64px",
-                // fontWeight: "bold",
-              }
-            }>
-            LOG IN
-          </ListItem>
-        </ListItemButton>
-      </Link>
+// export default function Actions() {
+//   return (
+//     <MyList type="row" sx={{ position: "absolute", right: "0", pt: 1 }}>
+//       <Link href="/log-in">
+//         <ListItemButton
+//           variant="contained"
+//           disableTouchRipple
+//           sx={{
+//             m: "4px",
+//             // },
+//           }}>
+//           <ListItem>LOG IN</ListItem>
+//         </ListItemButton>
+//       </Link>
 
-      <ListItemButton
-        variant="contained"
-        disableTouchRipple
-        sx={{
-          // justifyContent: "center",
-          "&.MuiListItemButton-root:hover": {
-            color: Colors.white_off, // check the color
-            background: "transparent",
-          },
-        }}>
-        <ListItem
-          sx={
-            {
-              // display: "flex",
-              // justifyContent: "center",
-              // fontWeight: "bold",
-              // background: Colors.green,
-              // borderRadius: "4px",
-              // letterSpacing: "0.64px",
-              // fontWeight: "bold",
-            }
-          }>
-          SIGN UP
-        </ListItem>
-      </ListItemButton>
-    </MyList>
-  );
-}
+//       <ListItemButton
+//         variant="contained"
+//         disableTouchRipple
+//         sx={{
+//           "&.MuiListItemButton-root:hover": {
+//             color: Colors.white_off,
+//             background: "transparent",
+//           },
+//         }}>
+//         <ListItem>SIGN UP</ListItem>
+//       </ListItemButton>
+//     </MyList>
+//   );
+// }
--- src/components/appbar/appbarDesktop.jsx
+++ src/components/appbar/appbarDesktop.jsx
@@ -1,81 +1,46 @@
 "use client";
 
-import { AppbarContainer, AppbarHeader, MyList } from "@/styles/appbar";
-import { ListItemButton } from "@mui/material";
+import {
+  AppbarContainer,
+  AppbarHeader,
+  MyList,
+  NavListItem,
+} from "@/styles/appbar";
 import { Colors } from "../../styles/theme";
 import Link from "next/link";
 import LoginButton from "./loginButton";
 // import { useAppSelector, useAppDispatch } from "@/redux/hooks";
 // import { setAnchorEl } from "@/redux/features/anchorEl/anchorSlice";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function AppbarDesktop({}) {
   const { data: session } = useSession();
+  const router = useRouter();
+
+  const handleButtonClick = (route) => {
+    router.push(route);
+  };
+
   return (
     <>
-      <AppbarContainer
-        sx={{
-          borderBottom: 3,
-          borderBottomColor: Colors.light_grey,
-        }}>
+      <AppbarContainer>
         <AppbarHeader>
-          <Link href="/">
-            <span
-              style={{
-                fontFamily: "",
-                fontSize: "34px",
-                fontWeight: "bold",
-                letterSpacing: "2px",
-                textShadow: "2px 2px 4px rgba(0, 0, 0, 0.1)",
-                paddingLeft: "1.6rem",
-              }}>
-              Trade<strong style={{ color: Colors.green }}>Tracker</strong>{" "}
-            </span>
+          <Link
+            href="/"
+            style={{
+              fontSize: "34px",
+            }}>
+            Trade<strong style={{ color: Colors.green }}>Tracker</strong>{" "}
           </Link>
         </AppbarHeader>
         <MyList type="row" sx={{}}>
-          <ListItemButton
-            variant="contained"
-            disableTouchRipple
-            sx={{
-              color: Colors.secondary,
-              "&.MuiListItemButton-root:hover": {
-                color: Colors.green,
-                background: "transparent",
-              },
-            }}>
-            <Link
-              href="/membership"
-              style={{
-                fontSize: "18px",
-                letterSpacing: "0.64px",
-                fontWeight: "bold",
-              }}>
-              MEMBERSHIP
-            </Link>
-          </ListItemButton>
-          <ListItemButton
-            variant="contained"
-            disableTouchRipple
-            sx={{
-              color: Colors.secondary,
-              "&.MuiListItemButton-root:hover": {
-                color: Colors.green,
-                background: "transparent",
-              },
-            }}>
-            <Link
-              href="/help"
-              style={{
-                fontSize: "18px",
-                letterSpacing: "0.64px",
-                fontWeight: "bold",
-              }}>
-              BLOG
-            </Link>
-          </ListItemButton>
-
-          {/* LOGIN IN */}
+          <NavListItem onClick={() => handleButtonClick("/membership")}>
+            MEMBERSHIP
+          </NavListItem>
+          <NavListItem onClick={() => handleButtonClick("/help")}>
+            BLOG
+          </NavListItem>
           <LoginButton session={session} />
         </MyList>
       </AppbarContainer>
